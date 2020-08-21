0ba070c651c06887a4177a1979247aaffc3d090e
krishna-incrypto <vpkrishnanand@gmail.com>
2020-08-21 13:05:36 +0530

    uid.ejs changeed

--- imagegen.js
+++ imagegen.js
@@ -4,16 +4,16 @@ var fileName = "public/images/poster.jpg";
 var imageCaption = "Image caption";
 var loadedImage;
 
-var waterMarkImage = async (fileName)  => {
-    var buff;
-    var base64;
-    var img = await Jimp.read(fileName)
-    var font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE)
-    await img.print(font,20,20,imageCaption).getBase64(Jimp.MIME_JPEG,(err,src)=>{
-        base64=src
-    })
- 
- return base64
+var waterMarkImage = async (fileName) => {
+  var buff;
+  var base64;
+  var img = await Jimp.read(fileName);
+  var font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+  base64 = await img
+    .print(font, 20, 20, imageCaption)
+    .getBase64Async(Jimp.MIME_JPEG);
+
+  return base64;
 };
 
 module.exports = waterMarkImage;
