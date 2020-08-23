238293d5b28a9703e7dad1da2381d30c5497e70e
krishna-incrypto <vpkrishnanand@gmail.com>
2020-08-23 18:07:29 +0530

    claim status check done

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ app.get("/", (req, res) => {
   res.redirect("/adstories");
 });
 var port = process.env.PORT || 3000;
+
+
 app.listen(port, () => {
   console.log("listening on http://localhost:" + port);
 });
