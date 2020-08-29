9b47cd1b9622e23509ee8d98d9816e4c75bd4af7
krishna-incrypto <vpkrishnanand@gmail.com>
2020-08-29 09:22:03 +0530

    connection string set

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     // mongoURI:"mongodb+srv://incrypt0:Anand@2000@adstories.agqn0.mongodb.net/adstories?retryWrites=true&w=majority",
-    mongoURI: "mongodb://localhost:27017" ||process.env.MONGO_URI || process.env.MONGO_DB_URI, 
+    mongoURI: "mongodb://localhost:27017/adstories" ||process.env.MONGO_URI || process.env.MONGO_DB_URI, 
 } 
